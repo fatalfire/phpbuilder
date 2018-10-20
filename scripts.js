dd22484bb13c59f0f0415a1c820b66eda35443e9
fatalfire <killman2@mail.ru>
2018-10-20 23:28:10 +0300

    js + google docs

--- scripts.js
+++ scripts.js
@@ -1,17 +1,34 @@
-function getSum(){
-    var sum = 0;
-    var number = 0;
-    while(number !== false){
-        number = prompt('Enter sum number\nNow you have: ' + sum);
-        if(isNaN(number) || number === ''){
-            alert("Error");
-            continue;
-        }else if (number === null){
-            break;
+window.onload = function(){
+    let getJSON = function(url, callback){
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.responseType = "json";
+        xhr.onload = function(){
+            let status = xhr.status;
+            if(status === 200){
+                callback(null, xhr.response);
+            }else{
+                callback(status, xhr.response);
+            }
+        };
+        xhr.send();
+    };
+
+    getJSON("https://spreadsheets.google.com/feeds/list/1MFDyCPZSExwL2lZzjNnHqwhiLAnqIPLV1BE2y0ru2n0/od6/public/values?alt=json", function(err, data){
+        if(err !== null){
+            alert("Error" + err);
+        }else{
+            data = data["feed"]["entry"];
+            console.log(data);
+            document.querySelector(".showGoods").innerHTML = showGoods(data);
         }
-        sum += parseInt(number);
+    });
+
+    function showGoods(data){
+        let out = "";
+        for(var i = 0; i<data.length; i++){
+            out += "<div><p>" + data[i]['gsx$id']['$t'] + "</p><p>" + data[i]['gsx$text']['$t'] + "</p></div>";
+        }
+        return out;
     }
-    return sum;
-}
-var result = getSum();
-console.log("Your sum is: " + result);
+};
